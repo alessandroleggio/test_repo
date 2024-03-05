1b23b185ca6232f36d968374e8945a50c42eddcc
Alessandro Leggio Yepez <aleggio1@jhu.edu>
2024-03-04 21:07:40 -0800

    yes
    yes

---
+++ app1.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.write("""
+# Streamlit can use markdown syntax
+## Level 2
++ bullet 1
++ bullet 2
+"""
+)
---
+++ multipage/home.py
@@ -0,0 +1,13 @@
+#home.py
+
+import streamlit as st
+
+st.write("""
+# This is the home page
+## The file structure looks lke this
++ 'home.py' #Thid is is the file you run with "streamlit run"
++ 'pages' a directory
++ 'page2.py' a file in 'pages'
++ 'page3.py' a file in 'pages'
+"""
+)
